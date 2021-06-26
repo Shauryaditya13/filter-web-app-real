b748740b54007ef3f1073e8f6281b8898c74b761
Shauryaditya13 <71700044+Shauryaditya13@users.noreply.github.com>
2021-06-26 16:55:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+nosex="";
+nosey="";
+
+function preload() {
+}
+
+function setup() {
+    canvas=createCanvas(500,500);
+    canvas.center();
+}
+
+function TakaSnapshot() {
+    save("MyFilter.png");
+}
+
+function draw() {
+}
